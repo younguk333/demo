1b777f14c79518d90d1440e2a68a21762619d466
younguk <younguk333@gmail.com>
2022-12-07 22:35:31 +0900

    homecontroller

---
+++ src/main/java/com/example/demo/web/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.demo.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello(){
+        System.out.println("hello?????");
+        return "hello";
+    }
+}
